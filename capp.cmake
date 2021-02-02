d9c2683171891320a3358b304b88fbfdc8612a3f
Dan Ibanez <daibane@sandia.gov>
2021-02-02 13:19:20 -0700

    trying to encapsulate configure as a function

--- capp.cmake
+++ capp.cmake
@@ -1,11 +1,11 @@
 message("CMAKE_CURRENT_SOURCE_DIR=${CMAKE_CURRENT_SOURCE_DIR}")
-set(ROOT "${CMAKE_CURRENT_SOURCE_DIR}")
-set(SOURCE_ROOT "${ROOT}/source")
-set(BUILD_ROOT "${ROOT}/build")
-set(INSTALL_ROOT "${ROOT}/install")
-make_directory("${SOURCE_ROOT}")
-make_directory("${BUILD_ROOT}")
-make_directory("${INSTALL_ROOT}")
+set(CAPP_ROOT "${CMAKE_CURRENT_SOURCE_DIR}")
+set(CAPP_SOURCE_ROOT "${CAPP_ROOT}/source")
+set(CAPP_BUILD_ROOT "${CAPP_ROOT}/build")
+set(CAPP_INSTALL_ROOT "${CAPP_ROOT}/install")
+make_directory("${CAPP_SOURCE_ROOT}")
+make_directory("${CAPP_BUILD_ROOT}")
+make_directory("${CAPP_INSTALL_ROOT}")
 set(CMAKE_PROGRAM_PATH "C:/Program Files") #workaround a workaround for MSVC 2017 in FindGit.cmake
 find_package(Git REQUIRED)
 message("Git is at ${GIT_EXECUTABLE}")
@@ -14,6 +14,7 @@ function(capp_execute)
   cmake_parse_arguments(PARSE_ARGV 0 capp_execute "" "WORKING_DIRECTORY;RESULT_VARIABLE" "COMMAND")
   string(REPLACE ";" " " capp_execute_printable "${capp_execute_COMMAND}")
   message("executing ${capp_execute_printable}")
+  message("in ${capp_execute_WORKING_DIRECTORY}")
   execute_process(
       COMMAND ${capp_execute_COMMAND}
       WORKING_DIRECTORY "${capp_execute_WORKING_DIRECTORY}"
@@ -33,7 +34,7 @@ function(capp_checkout)
   cmake_parse_arguments(PARSE_ARGV 0 capp_checkout "" "DIRECTORY;COMMIT;RESULT_VARIABLE" "")
   capp_execute(
     COMMAND "${GIT_EXECUTABLE}" checkout ${capp_checkout_COMMIT}
-    WORKING_DIRECTORY "${SOURCE_ROOT}/${capp_checkout_DIRECTORY}"
+    WORKING_DIRECTORY "${CAPP_SOURCE_ROOT}/${capp_checkout_DIRECTORY}"
     RESULT_VARIABLE git_checkout_result
     )
   set(${capp_checkout_RESULT_VARIABLE} "${git_checkout_result}" PARENT_SCOPE)
@@ -43,7 +44,7 @@ function(capp_clone)
   cmake_parse_arguments(PARSE_ARGV 0 capp_clone "" "DIRECTORY;GIT_URL;COMMIT;RESULT_VARIABLE" "")
   capp_execute(
     COMMAND "${GIT_EXECUTABLE}" clone -n ${capp_clone_GIT_URL} ${capp_clone_DIRECTORY}
-    WORKING_DIRECTORY "${SOURCE_ROOT}"
+    WORKING_DIRECTORY "${CAPP_SOURCE_ROOT}"
     RESULT_VARIABLE git_clone_result
     )
   if (NOT git_clone_result EQUAL 0)
@@ -65,11 +66,18 @@ capp_clone(
     RESULT_VARIABLE capp_clone_result
 )
 
-capp_execute(
-    COMMAND
-    "${CMAKE_COMMAND}"
-    "${SOURCE_ROOT}/trivial-mpi"
-    "-DCMAKE_INSTALL_PREFIX=${INSTALL_ROOT}/trivial-mpi"
-    WORKING_DIRECTORY "${BUILD_ROOT}/trivial-mpi"
-    RESULT_VARIABLE capp_configure_result
-)
+function(capp_configure)
+  cmake_parse_arguments(PARSE_ARGV 0 capp_configure "" "DIRECTORY;RESULT_VARIABLE" "OPTIONS")
+  capp_execute(
+      COMMAND
+      "${CMAKE_COMMAND}"
+      "${CAPP_SOURCE_ROOT}/${capp_configure_DIRECTORY}"
+      "-DCMAKE_INSTALL_PREFIX=${CAPP_INSTALL_ROOT}/${capp_configure_DIRECTORY}"
+      ${capp_configure_OPTIONS}
+      WORKING_DIRECTORY "${CAPP_BUILD_ROOT}/${capp_configure_DIRECTORY}"
+      RESULT_VARIABLE cmake_configure_result
+  )
+  set(${capp_configure_RESULT_VARIABLE} "${cmake_configure_result}" PARENT_SCOPE)
+endfunction()
+
+capp_configure(DIRECTORY trivial-mpi RESULT_VARIABLE capp_configure_result)
