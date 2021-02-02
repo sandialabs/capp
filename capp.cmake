420e124556d0d3dd59e8b3197bd9db6bae794449
Dan Ibanez <daibane@sandia.gov>
2021-02-02 14:26:27 -0700

    starting to use package variables

--- capp.cmake
+++ capp.cmake
@@ -1,4 +1,5 @@
 message("CMAKE_CURRENT_SOURCE_DIR=${CMAKE_CURRENT_SOURCE_DIR}")
+set(CAPP_BUILD_TYPE Release)
 set(CAPP_ROOT "${CMAKE_CURRENT_SOURCE_DIR}")
 set(CAPP_SOURCE_ROOT "${CAPP_ROOT}/source")
 set(CAPP_BUILD_ROOT "${CAPP_ROOT}/build")
@@ -74,14 +75,79 @@ function(capp_configure)
   set(${capp_configure_RESULT_VARIABLE} "${cmake_configure_result}" PARENT_SCOPE)
 endfunction()
 
-capp_clone(
-    DIRECTORY trivial-mpi
+function(capp_build)
+  cmake_parse_arguments(PARSE_ARGV 0 capp_build "" "DIRECTORY;RESULT_VARIABLE" "")
+  capp_execute(
+      COMMAND
+      "${CMAKE_COMMAND}"
+      "--build"
+      "."
+      "--config"
+      ${CAPP_BUILD_TYPE}
+      WORKING_DIRECTORY "${CAPP_BUILD_ROOT}/${capp_build_DIRECTORY}"
+      RESULT_VARIABLE cmake_build_result
+  )
+  set(${capp_build_RESULT_VARIABLE} "${cmake_build_result}" PARENT_SCOPE)
+endfunction()
+
+function(capp_install)
+  cmake_parse_arguments(PARSE_ARGV 0 capp_install "" "DIRECTORY;RESULT_VARIABLE" "")
+  capp_execute(
+      COMMAND
+      "${CMAKE_COMMAND}"
+      "--install"
+      "."
+      "--config"
+      ${CAPP_BUILD_TYPE}
+      WORKING_DIRECTORY "${CAPP_BUILD_ROOT}/${capp_install_DIRECTORY}"
+      RESULT_VARIABLE cmake_install_result
+  )
+  set(${capp_install_RESULT_VARIABLE} "${cmake_install_result}" PARENT_SCOPE)
+endfunction()
+
+function(capp_package)
+  cmake_parse_arguments(PARSE_ARGV 0 capp_package "" "NAME;GIT_URL;COMMIT" "OPTIONS")
+  set(${capp_package_NAME}_GIT_URL ${capp_package_GIT_URL} PARENT_SCOPE)
+  set(${capp_package_NAME}_COMMIT ${capp_package_COMMIT} PARENT_SCOPE)
+  set(${capp_package_NAME}_OPTIONS ${capp_package_OPTIONS} PARENT_SCOPE)
+  set(${capp_package_NAME}_DIRECTORY "${CAPP_PACKAGE_DIRECTORY}" PARENT_SCOPE)
+endfunction()
+
+function(capp_clone_package)
+  cmake_parse_arguments(PARSE_ARGV 0 capp_clone_package "" "NAME;RESULT_VARIABLE" "")
+  capp_clone(
+      DIRECTORY ${${capp_clone_package_NAME}_DIRECTORY}
+      GIT_URL ${${capp_clone_package_NAME}_GIT_URL}
+      COMMIT ${${capp_clone_package_NAME}_COMMIT}
+      RESULT_VARIABLE capp_clone_result
+  )
+  set(${capp_clone_package_RESULT_VARIABLE} ${capp_clone_result} PARENT_SCOPE)
+endfunction()
+
+set(CAPP_PACKAGE_DIRECTORY trivial-mpi)
+capp_package(
+    NAME TrivialMPI
     GIT_URL git@cee-gitlab.sandia.gov:daibane/trivial-mpi.git
-    COMMIT 137a5faf53f48d9a59ef24b05ec549ca39f77765
-    RESULT_VARIABLE capp_clone_result
+    COMMIT a340978950cc0a455abeb7d34b2812d8400bbf46
+)
+message("now TrivialMPI_GIT_URL is ${TrivialMPI_GIT_URL}")
+
+capp_clone_package(
+    NAME TrivialMPI
+    RESULT_VARIABLE capp_clone_package_result
 )
 
 capp_configure(
     DIRECTORY trivial-mpi
     RESULT_VARIABLE capp_configure_result
     )
+
+capp_build(
+    DIRECTORY trivial-mpi
+    RESULT_VARIABLE capp_build_result
+)
+
+capp_install(
+    DIRECTORY trivial-mpi
+    RESULT_VARIABLE capp_install_result
+)
