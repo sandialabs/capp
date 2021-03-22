f74cfdab24da1c6fb1adf89429a4f10488446007
Dan Ibanez <daibane@sandia.gov>
2021-03-22 08:27:58 -0600

    prevent commit from causing reconfigures

--- capp.cmake
+++ capp.cmake
@@ -537,7 +537,10 @@ function(capp_commit_command)
   file(READ "${CAPP_PACKAGE_ROOT}/${capp_commit_command_PACKAGE}/package.cmake" old_package_contents)
   string(REGEX REPLACE "COMMIT [a-z0-9]+" "COMMIT ${new_commit}" commit_package_contents "${old_package_contents}")
   string(REGEX REPLACE "GIT_URL [_a-zA-Z0-9:@/\\.-]+" "GIT_URL ${new_git_url}" new_package_contents "${commit_package_contents}")
-  file(WRITE "${CAPP_PACKAGE_ROOT}/${capp_commit_command_PACKAGE}/package.cmake" "${new_package_contents}")
+  # if the file doesn't change, don't write to it because that will trigger reconfiguration later
+  if (NOT old_package_contents STREQUAL new_package_contents)
+    file(WRITE "${CAPP_PACKAGE_ROOT}/${capp_commit_command_PACKAGE}/package.cmake" "${new_package_contents}")
+  endif()
   set(${capp_commit_command_RESULT_VARIABLE} 0 PARENT_SCOPE)
   capp_add_file(
     FILE "${CAPP_PACKAGE_ROOT}/${capp_commit_command_PACKAGE}/package.cmake"
