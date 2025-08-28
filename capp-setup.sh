d48afff117b7dd7221caedda2196cde30952db6f
Dan Ibanez <daibane@sandia.gov>
2025-08-28 08:51:04 -0600

    tweak how CApp is run with load/unload

--- capp-setup.sh
+++ capp-setup.sh
@@ -1,13 +1,15 @@
 export CAPP_ROOT=$PWD
 capp () {
   # Convert the Bash function arguments into a semicolon-separated CMake list
-  arglist="$(printf ';%q' "$@")"
-  stdout="$(cmake -DCAPP_CMDLINE_ARGS="$arglist" -P "$CAPP_ROOT/capp.cmake")" || return
+  arglist="$(printf ';%s' "$@")"
   case $1 in
     "load"|"unload")
+      stdout="$(cmake -DCAPP_CMDLINE_ARGS="$arglist" -P "$CAPP_ROOT/capp.cmake")" || return
       eval "$stdout"
+      unset stdout
       ;;
     *)
-      echo "$stdout"
+      cmake -DCAPP_CMDLINE_ARGS="$arglist" -P "$CAPP_ROOT/capp.cmake"
+      ;;
   esac
 }
